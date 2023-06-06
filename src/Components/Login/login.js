ca2ad42e404be04e059692bea987229b662a63ec
Meshkat <meshkatuddinahammed@outlook.com>
2023-06-06 18:38:19 +0600

    UI for the authentication system

---
+++ src/Components/Login/login.js
@@ -0,0 +1,21 @@
+import { Button, Text, TextInput, View, StyleSheet } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+      <Button title="Switch to Login"/>
+      <TextInput placeholder='Username or E-mail' style={styles.input}/>
+      <TextInput placeholder='Password' style={styles.input}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 500,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+});
